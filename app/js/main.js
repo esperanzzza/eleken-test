082e4623667ce68744c2201a17b14a8a0856b994
esperanzzza <veronikatelychko@yahoo.com>
2017-04-17 00:13:24 +0300

    section subscibe added; language and currency choice menu changed

--- app/js/main.js
+++ app/js/main.js
@@ -3,23 +3,27 @@ $(document).ready(function() {
     var
         toggleDown = $('.header__toggle-down'),
         menuChoice = $('.header__choice-menu');
-        menuChoiceCurrency = $(".header__choice-menu[data-menu='currency']"),
+    menuChoiceCurrency = $(".header__choice-menu[data-menu='currency']"),
         menuChoiceLanguage = $(".header__choice-menu[data-menu='lang']"),
         menuChoiceToggle = 'header__choice-menu--js',
         itemDefaultCurrency = $('.header__choice--currency'),
         itemDefaultLanguage = $('.header__choice--language'),
         itemChosen = $('.header__choice'),
+        menuClose = $('.header__close-icon'),
         toggleSearch = $('.header__search-icon'),
-        formSearch = $('.header__search-field'),
-        formRow = $('.header__right-row'),
-        formRowShow = 'header__right-row--js';
+        formSearch = $('.header__search-field');
 
     toggleDown.click(function(event) {
+        
         if ($(event.target).is("[data-menu='currency']")) {
             menuChoiceCurrency.addClass(menuChoiceToggle);
 
             itemChosen.click(function(event) {
                 itemDefaultCurrency.text($(event.target).attr('data-currency'));
+                menuChoice.removeClass(menuChoiceToggle);
+            });
+
+            menuClose.click(function() {
                 menuChoice.removeClass(menuChoiceToggle);
             });
 
@@ -31,6 +35,11 @@ $(document).ready(function() {
                 itemDefaultLanguage.text($(event.target).attr('data-lang'));
                 menuChoice.removeClass(menuChoiceToggle);
             });
+
+            menuClose.click(function() {
+                menuChoice.removeClass(menuChoiceToggle);
+            });
+
         }
 
     });
@@ -39,6 +48,6 @@ $(document).ready(function() {
         formSearch.focus();
 
 
-});
+    });
 
 });
