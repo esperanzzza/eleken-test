5aa2c15c7f1eb99cf7af669752917216eefb98c4
esperanzzza <veronikatelychko@yahoo.com>
2017-04-20 16:18:08 +0300

    updated header search form; improved hover styles

--- app/js/main.js
+++ app/js/main.js
@@ -3,22 +3,26 @@ $(document).ready(function() {
     var
         toggleDown = $('.header__toggle-down, .menu-mobile__toggle-down'),
         menuChoice = $('.header__choice-menu, .menu-mobile__choice-menu');
-        menuChoiceCurrency = $(".header__choice-menu[data-menu='currency'], .menu-mobile__choice-menu[data-menu='currency']"),
+    menuChoiceCurrency = $(".header__choice-menu[data-menu='currency'], .menu-mobile__choice-menu[data-menu='currency']"),
         menuChoiceLanguage = $(".header__choice-menu[data-menu='lang'], .menu-mobile__choice-menu[data-menu='lang']"),
         menuChoiceToggle = 'header__choice-menu--js, menu-mobile__choice-menu--js',
         itemDefaultCurrency = $('.header__choice--currency, .menu-mobile__choice--currency'),
         itemDefaultLanguage = $('.header__choice--language, .menu-mobile__choice--language'),
         itemChosen = $('.header__choice, .menu-mobile__choice'),
         menuClose = $('.header__close-icon, .menu-mobile__close-icon'),
-        toggleSearch = $('.header__search-icon, .menu-mobile__search-icon'),
-        searchClose = 'header__search-icon--close, menu-mobile__search-icon--close',
-        formSearch = $('.header__search-field, .menu-mobile__search-field'),
-        searchSubmit = $('.header__search-submit, .menu-mobile__search-submit'),
-        searchSubmitToggle = 'header__search-submit--active, menu-mobile__search-submit--active',
+        toggleSearch = $('.header__search-icon'),
+        toggleSearchMobile = $('.menu-mobile__search-icon'),
+        searchClose = 'menu-mobile__search-icon--close',
+        formSearch = $('.menu-mobile__search-field'),
+        searchSubmit = $('.menu-mobile__search-submit'),
+        searchSubmitToggle = 'menu-mobile__search-submit--active',
         burgerToggle = $('.burger'),
         burgerActive = 'burger--active',
         modal = $('.modal'),
+        modalSearch = $('.search'),
         modalOpen = 'modal--open',
+        searchShow = 'search--show',
+        searchHide = $('.search__close'),
         catalogItem = $('.catalog__item'),
         itemCurrent = 'catalog__item--current',
         left = $('.catalog__control--left'),
@@ -61,15 +65,26 @@ $(document).ready(function() {
 
     });
 
-    toggleSearch.click(function() {
+    toggleSearchMobile.click(function() {
         formSearch.focus();
-        toggleSearch.addClass(searchClose) && searchSubmit.addClass(searchSubmitToggle);
+        toggleSearchMobile.addClass(searchClose) && searchSubmit.addClass(searchSubmitToggle);
     });
 
+
+    toggleSearch.click(function() {
+        modalSearch.addClass(searchShow);
+    });
+
+    searchHide.click(function() {
+        modalSearch.removeClass(searchShow);
+    });
 
     burgerToggle.click(function() {
 
         $(this).toggleClass(burgerActive) && modal.toggleClass(modalOpen);
+        if (modalSearch.hasClass(searchShow)) {
+            modalSearch.removeClass(searchShow);
+        }
 
     });
 
